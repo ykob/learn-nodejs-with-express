02009be69f927285eaaaf6f84e1ed8151d450939
ykob <info@tplh.net>
2023-12-07 23:10:39 +0900

    added get users root.

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import express from "express";
 
+const prisma = new PrismaClient();
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.get("/users", async (req, res) => {
+  const users = await prisma.user.findMany();
+  res.json({ users });
 });
 
 app.listen(port, () => {
