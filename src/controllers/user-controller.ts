12b2354cd377908a12bbe08f2d600913a6acf17a
ykob <info@tplh.net>
2023-12-11 20:59:45 +0900

    revert to split router instance.

--- src/controllers/user-controller.ts
+++ src/controllers/user-controller.ts
@@ -1,7 +1,8 @@
 import { genSaltSync, hashSync } from "bcryptjs";
-import { Request, Response } from "express";
+import { Request, Response, Router } from "express";
 import { prisma } from "../db";
-import { router } from "../router";
+
+const router = Router();
 
 router.get("/", async (req: Request, res: Response) => {
   const users = await prisma.user.findMany();
