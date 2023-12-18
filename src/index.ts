b39019acee46f6392c42921736ae854e7f632aa0
ykob <info@tplh.net>
2023-12-18 18:04:53 +0900

    import auth routes on app index.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import auth from "./api/auth/auth.routes";
 import users from "./api/users/users.routes";
 
 const app = express();
@@ -9,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.use("/auth", auth);
 app.use("/users", users);
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
